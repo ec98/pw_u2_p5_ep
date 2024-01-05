fd415c645354fce2c0a79804e375d8d48ad8b59e
ec98 <carlospiruch98@gmail.com>
2024-01-04 19:38:48 -0500

    declaracion de props y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
+  <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <Contador titulo="Contador 1" />
-    <Contador titulo="Contador 2"/>
-    <Contador titulo="Contador 3"/>
+    <Contador titulo="Contador 1" :valor = '10'/>
+    <Contador titulo="Contador 2" v-bind:valor = "calcularValor()"/>
+    <Contador titulo="Probador"/>
+    <!-- <Contador titulo="Contador 3" valor = "ocho"/> -->
+  </div>
 </template>
 
 <script>
@@ -11,7 +14,12 @@ import Contador from "./components/Contador.vue";
 export default {
   name: "App",
   components: {
-    Contador
+    Contador,
+  },
+  methods:{
+    calcularValor(){
+      return 5 + 5 * 100;
+    }
   }
 };
 </script>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -4,37 +4,66 @@
     <h2>{{ titulo }}</h2>
     <!-- <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
     <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
-    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p> -->
+    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>-->
     <!-- <p>{{ counter }} <sup>2</sup> = {{ operacionCuadrada() }}</p>
     <p>{{ counter }} <sup>2</sup> = {{ operacionCuadrada() }}</p>
-    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadrada() }}</p> -->
+    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadrada() }}</p>-->
+    <!-- <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
     <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
     <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
     <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
-    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
-    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
-    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>
-    
+    <p>{{ counter }} <sup>2</sup> = {{ operacionCuadradaComputada }}</p>-->
+    <p>
+      {{ counter }}
+      <sup>2</sup>
+      = {{ operacionCuadradaComputada }}
+    </p>
+    <button @click="suma()">+1</button>
+    <button @click="resta()">-1</button>
   </div>
 </template>
 
 <script>
 export default {
-  props:['titulo'],
-  data(){
-    return {
-      counter: 5,
+  // props:['titulo', 'valor'],
+  props: {
+    titulo: String,
+    // titulo:{
+    //   type: String,
+    //   required: true,
+    // },
+    // valor: Number,
+    valor: {
+      type: Number,
+      required: false, //si es siempre requerido
+      default: 100, //si no se envia el valor.
+      validator(value) {
+        //SIEMPRE DEBE RETORNAR UN BOOLEANO.
+        // console.log("error");
+        return value > 0 ? true : false;
+      }
     }
   },
-  methods:{
-    operacionCuadrada(){
-      console.log("Se calcula el cuadrado -> ",this.counter);
+  data() {
+    return {
+      counter: this.valor //padre
+    };
+  },
+  methods: {
+    operacionCuadrada() {
+      console.log("Se calcula el cuadrado -> ", this.counter);
       return this.counter * this.counter;
+    },
+    suma() {
+      return this.counter++;
+    },
+    resta() {
+      return this.counter--;
     }
   },
 
-  computed:{
-    operacionCuadradaComputada(){
+  computed: {
+    operacionCuadradaComputada() {
       console.log("Se calcula el cuadrado Computada");
       return this.counter * this.counter;
     }
@@ -42,5 +71,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+h2{
+  font-family: 'Times New Roman', Times, serif;
+}
+
+p{
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+/* *{
+font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+} */
+
+button{
+  background: rgb(81, 148, 142);
+  border-radius: 5px;
+  font-size: 21px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 4px;
+  padding: 2px 15px;
+}
+
+button:hover{
+  background: #aeb45e;
+}
+
 </style>
