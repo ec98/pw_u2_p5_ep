53db276f38c4a0b75cd1f440021192fd1b5a2c67
ec98 <carlospiruch98@gmail.com>
2024-01-06 10:58:40 -0500

    uso de apis relacionados con json

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Contador titulo="Contador 1" :valor = '10'/>
-    <Contador titulo="Contador 2" v-bind:valor = "calcularValor()"/>
-    <Contador titulo="Probador"/>
-    <!-- <Contador titulo="Contador 3" valor = "ocho"/> -->
+    <Pregunta></Pregunta>
   </div>
 </template>
 
 <script>
-import Contador from "./components/Contador.vue";
+import Pregunta from "./components/Pregunta.vue";
 
 export default {
   name: "App",
   components: {
-    Contador,
+    Pregunta,
   },
-  methods:{
-    calcularValor(){
-      return 5 + 5 * 100;
-    }
-  }
+  // methods:{
+  //   calcularValor(){
+  //     return 5 + 5 * 100;
+  //   }
+  // }
 };
 </script>
 
---
+++ src/components/Pregunta.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <img
+      src="https://yesno.wtf/assets/yes/8-2f93962e2ab24427df8589131da01a4d.gif"
+      alt="No se puede visualizar la imagen"
+    />
+    <div class="dark"></div>
+    <div class="container">
+      <input v-model="pregunta" type="text" />
+      <p>Recuerda que debes terminar con un signo de interogacion ('?')</p>
+      <h2>{{ pregunta }}</h2>
+      <h1>SI, NO...</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pregunta: "put your question :)",
+    };
+  },
+  methods: {
+    async consumirAPI() {
+      //se guarda la respuesta de API de tal respuesta.
+    //   const data = await fetch("https://yesno.wtf/api").then((respuesta) =>
+    //     respuesta.json()
+    //   );
+      const {answer, image} = await  fetch("https://yesno.wtf/api").then((respuesta) =>
+        respuesta.json()
+      );
+    //   console.log(data);
+        console.log(answer);
+        console.log(image);
+
+      //depende de la funcionalidad de la api porque necesita esperar una respuesta de la peticion.
+    },
+  },
+  watch: {
+    pregunta(value, oldValue) {
+      // console.log(value);
+      // console.log(oldValue);
+      if (!value.includes("?")) {
+        return;
+      }
+      //CONSUMO DEL API.
+      this.consumirAPI();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dark,
+img {
+  height: 100vh;
+  width: 100vw;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+}
+.dark {
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+input:focus {
+  outline: none;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+
+.container {
+  position: relative;
+}
+p {
+  color: rgb(209, 252, 68);
+  font-size: 20px;
+  margin-top: 10px;
+}
+h1,
+h2 {
+  color: white;
+}
+h2 {
+  margin-top: 170px;
+}
+</style>
