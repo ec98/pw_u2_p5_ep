89a8ad163c0cfbe048c19d181fa17c29545a3cdb
ec98 <carlospiruch98@gmail.com>
2024-01-17 20:39:18 -0500

    getAPI por relacion componentes y montar

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
     <h1>Adivina el pokemon</h1>
-    <PokemonImagen :pokemonId="4" :showPokemon="true"/>
-    <PokemonOpciones/>
+    <PokemonImagen :pokemonId="4" :showPokemon="true" />
+    <PokemonOpciones />
   </div>
 </template>
 
 <script>
-import PokemonImagen from '../components/PokemonImagen.vue'
-import PokemonOpciones from '../components/PokemonOpciones.vue'
+import PokemonImagen from "../components/PokemonImagen.vue";
+import PokemonOpciones from "../components/PokemonOpciones.vue";
 
-import obtenerIdPokemonFachada from '../helpers/pokemonHelper'
-
-obtenerIdPokemonFachada()
+import getPokemonsFachada from "../helpers/pokemonHelper";
 
 export default {
+  components: {
+    PokemonImagen,
+    PokemonOpciones
+  },
 
-    components:{
-        PokemonImagen,
-        PokemonOpciones
+  //montar el componente
+  mounted() {
+    console.log("se monto el componente pokemon page");
+    this.construirLoad();
+  },
+  methods: {
+    async construirLoad() {
+      const arrayPokemons = await getPokemonsFachada();
+      console.log("Usando el componente de arreglos -> " + arrayPokemons);
     }
-
+  }
 };
 </script>
 
