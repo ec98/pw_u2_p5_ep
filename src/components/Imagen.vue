18fd6627be086617dabe9145494a93fc535b0e5d
ec98 <carlospiruch98@gmail.com>
2024-01-18 20:40:39 -0500

    correccion del examen final sumativa 1

---
+++ src/components/Imagen.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <img :src="urlImg" alt="No se muestra imagen" />
+    <h1>{{ texto }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String
+    },
+    urlImg: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped>
+img {
+  width: 150px;
+  height: 200px;
+}
+h1 {
+  font-size: 20px;
+}
+
+</style>
